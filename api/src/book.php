e36d3bbc7201a629b0491e684475e4d7c5416e1e
Ewa Kozicka <ewa.kozicka@gmail.com>
2017-03-20 10:38:49 +0100

    Prepare connection to DB and add class Book

---
+++ api/src/book.php
@@ -0,0 +1,168 @@
+<?php
+
+require_once './connDB.php';
+
+class Book {
+
+    private $id, $name, $author, $description;
+
+    public function __construct() {
+        $this->id = -1;
+        $this->name = '';
+        $this->author = '';
+        $this->description = '';
+        
+        return $this;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+    
+    function getAuthor() {
+        return $this->author;
+    }
+
+    function getDescription() {
+        return $this->description;
+    }
+
+    function setName($name) {
+        if (!empty($name) && is_string($name) && strlen($name) < 255) {
+            $newName = strtoupper($name[0]).  substr($name, 1);
+            $this->name = $newName;
+        }
+    }
+
+    function setAuthor($author) {
+        if (!empty($author) && is_string($author) && strlen($author) < 255) {
+            $newAuthorArray = [];
+            $splittedAuthor = explode(" ", $author);
+            foreach ($splittedAuthor as $part) {
+                $newAuthorArray[] = strtoupper($part[0]).  substr($part, 1);
+            }
+            $newAuthor = implode(" ", $newAuthorArray);
+            
+            $this->author = $newAuthor;
+        }
+    }
+
+    function setDescription($description) {
+        if (!empty($description) && is_string($description)) {
+            $this->description = $description;
+        }
+    }
+
+    function create($conn, $name, $author, $description) {
+        if ($this->getId() == -1) {
+            
+            $this->setName($name);
+            $nameToAdd = $this->getName();
+            $this->setAuthor($author);
+            $authorToAdd = $this->getAuthor();
+            $this->setDescription($description);
+            $descriptionToAdd = $this->getDescription();
+            
+            $sql = "INSERT INTO `Book`(`name`, `author`, `description`) VALUES (:name, :author, :description);";
+            
+            try {
+                $query = $conn->prepare($sql);
+                $result = $query->execute([
+                    'name' => $nameToAdd,
+                    'author' => $authorToAdd,
+                    'description' => $descriptionToAdd,
+                ]);
+                
+                $this->id = $conn->lastInsertId();
+                
+                return $this;
+                
+            } catch (PDOException $ex) {
+                $ex->getMessage();
+            }
+        }
+    }
+
+    function update($conn, $name, $author, $description) {
+        if ($this->getId() > -1) {
+            
+            $sql = "UPDATE `Book` SET `name` = :name, `author` = :author, `description` = description WHERE `id` = :id;";
+            
+            try {
+                $query = $conn->prepare($sql);
+                $result = $query->execute([
+                    'name' => $name,
+                    'author' => $author,
+                    'description' => $description,
+                    'id' => $this->getId(),
+                ]);
+                
+                return $result;
+                
+            } catch (PDOException $ex) {
+                $ex->getMessage();
+                
+                return false;
+            }
+        }
+    }
+
+    function deleteFromDB($conn) {
+        if ($this->getId() !== -1) {
+            
+            $sql = "DELETE FROM `Book` WHERE `id` = :id;";
+            
+            try {
+                $query = $conn->prepare($sql);
+                $query->execute([
+                    'id' => $this->getId(),
+                ]);
+                    $this->id = -1;
+                    
+                    return true;
+                
+            } catch (PDOException $ex) {
+                $ex->getMessage();
+                
+                return false;
+            }
+            
+        } return true;
+    }
+    
+    
+    
+    function loadFromDB($conn, $id) {
+
+        $sql = 'Select * From `Book` WHERE `id` = :id';
+
+        try {
+            $query = $conn->prepare($sql);
+            $query->execute([
+                'id' => $id,
+                            ]);
+            $book = $query->fetch();
+            
+            $newBook = new Book();
+            $newBook->id = $book['id'];
+            $newBook->name = $book['name'];
+            $newBook->author = $book['author'];
+            $newBook->description = $book['description'];
+                
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+        }
+        
+        if ($book != null) {
+            return $newBook;
+        } else {
+            return NULL;
+        }
+    }
+}
+
+    
