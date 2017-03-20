d124c25ce6960f3cad799adfe38d50f46ff89ec1
Ewa Kozicka <ewa.kozicka@gmail.com>
2017-03-20 19:04:55 +0100

    create main site layout

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>REST-API CL Workshop</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="rest-api.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+            <div class="inner cover">
+                <h1 class="cover-heading">Manage your library</h1>
+                <h2 class="lead">Your books:</h2>
+            </div>
+                 
+<!--                 
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+            </p>-->
+        </div>
+         <div class="row">
+             
+                
+        </div>
+      </div>
+    </div>
+     
+      <script src="jquery-3.1.1.js"></script>
+      <script src="books.js"></script>
+  </body>
+</html>
